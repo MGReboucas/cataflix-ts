d00fdf10b2e191d551a07247994959dd0fb6096a
mgreboucas <reboucas444@gmail.com>
2025-11-19 09:45:25 -0300

    Upload initial project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="flex column">
+        <p className="text-2xl">Trabalho Front End AV2</p>
+        <p className="text-base">Trabalho Front End AV2</p>
+      </div>
+    </>
+  )
+}
+
+export default App
